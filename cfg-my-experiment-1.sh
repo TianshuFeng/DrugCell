37678132466f9049b96bb15dbb10abe6d23b1637
Rohan Gnanaolivu <ac.rgnanaolivu@lambda0.cels.anl.gov>
2024-02-22 13:00:41 -0800

    cfg ready for polaris

--- cfg-my-experiment-1.sh
+++ cfg-my-experiment-1.sh
@@ -3,8 +3,8 @@
 
 source_cfg -v cfg-my-settings.sh
 
-export CANDLE_DATA_DIR=/tmp/DrugCell/Data/
+export CANDLE_DATA_DIR=/home/rgnanaolivu/improve/DrugCell/data/
 export CANDLE_MODEL_TYPE="SINGULARITY"
 export PARAM_SET_FILE=my-nt3-search.json
-export MODEL_NAME=/homes/ac.rgnanaolivu/improve_data_dir/Singularity/images/DrugCell:0.0.1-20231029.sif
+export MODEL_NAME=/home/rgnanaolivu/improve/Singularity/DrugCell.sif
 
--- cfg-my-settings.sh
+++ cfg-my-settings.sh
@@ -1,24 +1,27 @@
 
+
+
 echo DrugCell SETTINGS
 echo SETTINGS
-PROCS=6
-export PROCS=6
 # General Settings
-export PPN=6
-export WALLTIME=48:00:00
-export NUM_ITERATIONS=1
-export POPULATION_SIZE=75
+export PROCS=10
+export PPN=5
+export WALLTIME=12:00:00
+export NUM_ITERATIONS=5
+export POPULATION_SIZE=98
 # GA Settings
-export STRATEGY='mu_plus_lambda'
-export OFF_PROP=0.5
+export GA_STRATEGY='mu_plus_lambda'
+export OFFSPRING_PROPORTION=0.5
 export MUT_PROB=0.8
 export CX_PROB=0.2
 export MUT_INDPB=0.5
 export CX_INDPB=0.5
-export TOURNAMENT_SIZE=4
+export TOURNSIZE=4
 # Lambda Settings
-export CANDLE_CUDA_OFFSET=4
-export CANDLE_DATA_DIR=/tmp/DrugCell/Data
+# export CANDLE_CUDA_OFFSET=1
+#export CANDLE_DATA_DIR=/tmp/rgnanaolivu
 # Polaris Settings
 # export QUEUE="debug"
-
+# Polaris Settings
+export QUEUE="prod"
+export CANDLE_DATA_DIR=/home/rgnanaolivu/improve/DrugCell/data/
